0b906cb15174f5076723d2b0dfdce5f2d5b58a7c
johnwilson <johnwilson3678@gmail.com>
2026-05-23 01:49:04 +0800

    Add GitHub Actions CI workflow, bump version to 1.4.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crypto4py",
-    version="1.4.0",
+    version="1.4.1",
     description="Python port of CryptoJS - standard and secure cryptographic algorithms",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
