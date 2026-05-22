33db720265507d45c558cf1855542f3f5738c7bc
johnwilson <johnwilson3678@gmail.com>
2026-05-23 00:48:37 +0800

    Fix language switch links to use absolute GitHub URLs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crypto4py",
-    version="1.2.0",
+    version="1.2.1",
     description="Python port of CryptoJS - standard and secure cryptographic algorithms",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
