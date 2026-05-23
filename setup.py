fe6f7637b3709d674237991b59dfa1f62dd984dc
johnwilson <johnwilson3678@gmail.com>
2026-05-23 22:14:15 +0800

    Bump version to 1.5.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="crypto4py",
-    version="1.5.1",
+    version="1.5.2",
     description="Python port of CryptoJS - standard and secure cryptographic algorithms",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
